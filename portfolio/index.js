76e1b8349d34ed40667698f8c3121d87770e70b0
Anilkumar426 <roddaanil2105@gmail.com>
2025-07-15 23:41:21 +0530

    password

---
+++ portfolio/index.js
@@ -0,0 +1,18 @@
+// Function to generate a random password
+function generatePassword(length) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*().";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+
+  return result;
+}
+
+// Call the function with desired password length
+const password = generatePassword(12); // You can change the number to any desired length
+
+// Log the generated password
+console.log("Generated password:"+password);
